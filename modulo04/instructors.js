34e3656f2df78e0082ddc670ca94cf8dff3bb5d9
Vitor Pontual <vitorpguedes@me.com>
2020-08-16 16:58:00 -0300

    Implementando datetime

--- modulo04/instructors.js
+++ modulo04/instructors.js
@@ -1,18 +1,31 @@
 const fs = require('fs')
-
+const data = require('./data')
 // create 
 
 exports.post = (req, res) => {
 
    const keys = Object.keys(req.body)
 
+   let { avatar_url, name, birth, gender, services } = req.body
+
    for( key in keys ) {
       if(req.body[key] == '') {
 	 return res.send('Please, fill all fields')
       }
    }
+   const id = Number(data.instructors.length + 1)
 
-   fs.writeFile('data.json', JSON.stringify(req.body), (err) => {
+   data.instructors.push({
+      id,
+      avatar_url,
+      name,
+      birth,
+      gender,
+      services,
+      
+   })
+
+   fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
       if(err) return res.send('Write File error!')
 
    return res.redirect('/instructors')
