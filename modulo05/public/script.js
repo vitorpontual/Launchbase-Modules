263b6caa66af62df6d5ccd2a264edb716e66da2f
Vitor Pontual <vitorpguedes@me.com>
2020-09-22 17:39:57 -0300

    Paginate Script

--- modulo05/public/script.js
+++ modulo05/public/script.js
@@ -7,3 +7,30 @@ for (item of menuItems) {
    } 
 }
 
+function pagination(selectedPage, totalPages){
+   let pages = [],
+      oldPage
+
+   for( let currentPage = 1; currentPage <= totalPages; currentPage++ ){
+      const firstAndLastPage = currentPage == 1 || currentPage == totalPages
+      const pagesAfterSelectedPage = currentPage <= selectedPage + 2
+      const pagesBeforeSelectedPage = currentPage  >= selectedPage - 2
+
+      if(firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage){
+	 console.log(currentPage)
+	 if( oldPage && currentPage - oldPage > 2 ){
+	    pages.push("...")
+	 }
+	 if (oldPage && currentPage - oldPage == 2){
+	    pages.push(oldPage + 1)
+	 }
+
+	 pages.push(currentPage)
+
+	 oldPage = currentPage
+	 
+      }
+   }
+   return pages
+}
+console.log(pagination(7, 10))
