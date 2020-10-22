407f0da4454eae0f5677275ca234abb5f6882764
Vitor Pontual <vitorpguedes@me.com>
2020-10-22 09:51:47 -0300

    uploadImage/ Script/ Style

--- modulo06/public/scripts.js
+++ modulo06/public/scripts.js
@@ -15,3 +15,40 @@ const Mask = {
    }
 }
 
+const PhotosUpload = {
+   uploadLimit: 6,
+
+   handleFileInput(event) {
+      console.log(event.target)
+      const { files: fileArr } = event.target
+      const { uploadLimit } = PhotosUpload
+
+      console.log(fileArr)
+      if (fileArr.length > uploadLimit) {
+	 alert(`Envie no máximo ${uploadLimit} photos`)
+	 event.preventDefault()
+	 return
+      }
+
+      Array.from(fileArr).forEach(file => {
+	 const reader = new FileReader()
+
+	 reader.onload = () => {
+	    const image = new Image()
+	    image.src = String(reader.result)
+
+	    const container = document.createElement('div')
+	    container.classList.add('photo')
+
+	    container.onclick = () => alert('remover foto')
+
+	    container.appendChild(image)
+
+	    document.querySelector('#photos-preview').appendChild(container)
+
+	 }
+
+	 reader.readAsDataURL(file)
+      })
+   }
+}
