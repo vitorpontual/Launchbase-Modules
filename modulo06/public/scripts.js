5684e59f04afbe7b6fc8f89dd6e777a06bfeea59
Vitor Pontual <vitorpguedes@me.com>
2020-10-28 22:36:08 -0300

    Logic Image Gallery

--- modulo06/public/scripts.js
+++ modulo06/public/scripts.js
@@ -116,5 +116,17 @@ const PhotosUpload = {
       photoDiv.remove()
    }
 
+}
 
+const ImageGallery = {
+   highlight: document.querySelector('.gallery .highlight > img'),
+   previews: document.querySelectorAll('.gallery-preview img'),
+   setImage(event){
+      const { target } = event
+
+      ImageGallery.previews.forEach(preview => preview.classList.remove('active'))
+      target.classList.add('active')
+
+      ImageGallery.highlight.src = target.src
+   }
 }
