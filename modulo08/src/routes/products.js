5305987c368b8d3d1b31eefe6ebdad5da7ab8761
Vitor Pontual <vitorpguedes@me.com>
2020-12-27 11:03:08 -0300

    Product Validate - front and back

--- modulo08/src/routes/products.js
+++ modulo08/src/routes/products.js
@@ -8,6 +8,8 @@ const multer = require('../app/middleware/multer')
 const ProductController = require('../app/controllers/ProductController')
 const SearchController = require('../app/controllers/SearchController')
 
+const Validator = require('../app/validators/product')
+
 // Search
 routes.get('/search', SearchController.index)
 
@@ -16,7 +18,7 @@ routes.get('/create', onlyUsers, ProductController.create)
 routes.get('/:id', ProductController.show)
 routes.get('/:id/edit', onlyUsers, ProductController.edit)
 
-routes.post('/', onlyUsers, multer.array('photos', 6), ProductController.post)
+routes.post('/', onlyUsers, multer.array('photos', 6), Validator.post, ProductController.post)
 routes.put('/', onlyUsers, multer.array('photos', 6), ProductController.put)
 routes.delete('/', onlyUsers, ProductController.delete)
 
